29d5b9a7827539fb7c4e505ad56b7736e8985c1c
SRIJAN SARKAR <srijansarkar1992@gmail.com>
2019-12-09 11:01:04 +0530

    Add files via upload

---
+++ bestindex.java
@@ -0,0 +1,49 @@
+import java .util.*;
+class bestindex
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the order");
+        int x=sc.nextInt();
+        int b[]=new int[x];
+        int sum=0;
+        for(int l=0;l<x;l++)
+        {
+            b[l]=sc.nextInt();    //-3 2 3 -4 3 1
+        }
+        for(int i=0;i<b.length;i++)
+        {
+            int t=b[i];
+            int n=2,s=0;
+            int k=i+1;
+            int len=b.length;
+            while(n<len)
+            {
+
+                for(int j=k;j<=n;j++)
+                {
+                    s=s+b[j];
+                }
+                k=k+n;
+                if(k<=b.length-2)
+                {
+                    n++;
+                    continue;
+                }
+                else
+                {
+                    s=s+b[i];
+                    break;
+                }
+                
+                
+                
+            }
+            sum=s+t;
+            System.out.println(sum);
+            len--;
+        }
+    }
+
+}

8e3a14cbd254661c992edf38fce85214a0805e85
SRIJAN SARKAR <srijansarkar1992@gmail.com>
2019-12-09 16:49:10 +0530

    Create amicableno.java

---
+++ amicableno.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class amicableno
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the nos& term");
+        int x=sc.nextInt();
+        int z=sc.nextInt();
+        int c=0;
+        do
+        {
+            
+            int s=isami(x);
+            if(isami(s)==x)
+            {
+                c++;
+                System.out.println(x);
+            }
+            x++;
+            
+           
+           
+        }while(c<z);
+        
+    }
+    static int isami(int n)
+    {
+        int s=0;
+        for(int i=1;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                 s=s+i;
+            }
+            
+        }
+        return s;
+    }
+    
+}
